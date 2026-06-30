2e172c376e08a196913419bcade333be331810f7
Austin (Ngoc Thang) Pham <austinpham@microsoft.com>
2026-06-30 21:44:23 +1000

    feat(wezterm): add WezTerm config
    JetBrainsMono Nerd Font Mono @ 12, Github Dark (Gogh) theme, 0.95
    background opacity, minimal window (integrated buttons, tab bar hidden
    at one tab), full-height padding, and system + visual bell.
    
    Signed-off-by: Austin (Ngoc Thang) Pham <austinpham@microsoft.com>

---
+++ .wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'JetBrainsMono Nerd Font Mono'
+config.font_size = 12
+config.line_height = 1.0
+
+config.color_scheme = 'Github Dark (Gogh)'
+config.window_background_opacity = 0.95
+
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = { left = 8, right = 8, top = 0, bottom = 0 }
+
+config.audible_bell = 'SystemBeep'
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 75,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 75,
+}
+config.colors = {
+  visual_bell = '#202020',
+}
+
+return config
